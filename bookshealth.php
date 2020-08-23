020d0488cf2a7121d1e231c4dd0f7ca23a85281c
Maryam-Tahir99 <61540672+Maryam-Tahir99@users.noreply.github.com>
2020-08-23 20:30:40 +0500

    Update bookshealth.php

--- bookshealth.php
+++ bookshealth.php
@@ -60,8 +60,9 @@ if (isset($_POST['search'])) {
         <a href="<?php echo $Result['books_link']; ?>">
             <img class="img-responsive img-thumbnail" src="./bootstrap/img/<?php echo $Result['book_image']; ?>">
             <a href="<?php echo $Result['books_link']; ?>" target="_self" class="btn btn-primary btn-read">Read</a>
-            <a href="download.php?file=<?php echo $Result['books_link']; ?>" class="btn btn-primary btn-down">Download </a>
-
+            <a href="download.php?file=<?php echo $Result['books_link']; ?>" download>
+                    <a class="btn btn-primary btn-down">Download </a>
+                </a>
 
         </a>
         <?php
@@ -94,7 +95,9 @@ if (isset($_POST['search'])) {
             <a href="./BOOKS/<?php echo $query_row['books_link']; ?>">
                 
                 <a href="./BOOKS/<?php echo $query_row['books_link']; ?>" target="_blank" class="btn btn-primary btn-resp">Read</a>
-                <a href="download.php?file=<?php echo $query_row['books_link']; ?>" class="btn btn-primary btn-down">Download </a>
+                <a href="download.php?file=<?php echo $Result['books_link']; ?>" download>
+                    <a class="btn btn-primary btn-down">Download </a>
+                </a>
             </a>
 
         </div>
