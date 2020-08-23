71d05adcca11f293d7237a0fdc15237f2cb878ac
Maryam-Tahir99 <61540672+Maryam-Tahir99@users.noreply.github.com>
2020-08-23 20:09:19 +0500

    Update download.php

--- download.php
+++ download.php
@@ -2,6 +2,7 @@
 <?php
 $file=$_GET['file'];
  header("Content-Disposition: attachment; filename=" . urlencode($file));   /*attachment for downloading file  */
+header("Content-Type: application/octet-stream");
 $fo=fopen($file,"r");
 while(!feof($fo)){
     echo fread($fo,8000);
