92872d712891257f357df5103e1c81ce46a96255
Maryam-Tahir99 <61540672+Maryam-Tahir99@users.noreply.github.com>
2020-08-23 21:01:45 +0500

    Update bookshealth.php

--- bookshealth.php
+++ bookshealth.php
@@ -59,7 +59,7 @@ if (isset($_POST['search'])) {
     <div class="col-md-3">
         <a href="<?php echo $Result['books_link']; ?>">
             <img class="img-responsive img-thumbnail" src="./bootstrap/img/<?php echo $Result['book_image']; ?>">
-            <a href="<?php echo $Result['books_link']; ?>" target="_self" class="btn btn-primary btn-read">Read</a>
+            <a href="<?php echo $Result['books_link']; ?>" target="_blank" class="btn btn-primary btn-read">Read</a>
             <a href="<?php echo $Result['books_link']; ?>" download>
                     <a class="btn btn-primary btn-down">Download </a>
                 </a>
