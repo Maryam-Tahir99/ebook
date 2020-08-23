efbd80b96cd6e94abf9f0572d03b9517a0eaefd1
Maryam-Tahir99 <61540672+Maryam-Tahir99@users.noreply.github.com>
2020-08-23 20:02:35 +0500

    Delete pdfdispl.php

--- pdfdispl.php
+++
@@ -1,9 +0,0 @@
-<?php
-$file=$query_row['books_link'];
-header("Content-type: application/pdf");
-header("Content-Disposition: inline; filename='" . $file ."'");   /*inline to open on the spot*/
-/** header("Content-Disposition: attachment; filename='" . $file ."'");   attachment for downloading file  */
-header("Content-Transfer-Encoding: binary");
-header("Accept-Ranges: bytes");
-@readfile($file);
-?>
