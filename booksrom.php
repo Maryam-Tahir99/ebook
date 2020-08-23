6cf2c1da776c851b8f851af4dde5721365e8d031
Maryam-Tahir99 <61540672+Maryam-Tahir99@users.noreply.github.com>
2020-08-23 21:28:00 +0500

    Update booksrom.php

--- booksrom.php
+++ booksrom.php
@@ -60,7 +60,7 @@ if (isset($_POST['search'])) {
         <a href="<?php echo $Result['books_link']; ?>">
             <img class="img-responsive img-thumbnail" src="./bootstrap/img/<?php echo $Result['book_image']; ?>">
             <a href="<?php echo $Result['books_link']; ?>" target="_self" class="btn btn-primary btn-read">Read</a>
-            <a href="download.php?file=<?php echo $Result['books_link']; ?>" class="btn btn-primary btn-down">Download </a>
+            <a href="./BOOKS/<?php echo $Result['books_link']; ?>" class="btn btn-primary btn-down" download target="_self">Download </a>
 
 
         </a>
@@ -91,7 +91,7 @@ if (isset($_POST['search'])) {
             <a href="./BOOKS/<?php echo $query_row['books_link']; ?>">
                 
                 <a href="./BOOKS/<?php echo $query_row['books_link']; ?>" target="_blank" class="btn btn-primary btn-resp">Read</a>
-                <a href="download.php?file=<?php echo $query_row['books_link']; ?>" class="btn btn-primary btn-down">Download </a>
+                <a href="./BOOKS/<?php echo $Result['books_link']; ?>" class="btn btn-primary btn-down" download target="_self">Download </a>
             </a>
 
         </div>
