b45d38fc21810b7e39bd8a7b5fe5c9e0a4cc9bb5
Maryam-Tahir99 <61540672+Maryam-Tahir99@users.noreply.github.com>
2020-08-23 20:54:22 +0500

    Update download.php

--- download.php
+++ download.php
@@ -1,13 +1,16 @@
 
-<?php if (isset($_GET['file'])) {
-$file = $_GET['file'];
-if (file_exists($file) && is_readable($file) && preg_match('/\.pdf$/',$file)) {
- header('Content-Type: application/pdf');
- header("Content-Disposition: attachment; filename=\"$file\"");
- readfile($file);
-}
-} else {
- header("HTTP/1.0 404 Not Found");
- echo "<h1>Error 404: File Not Found: <br /><em>$file</em></h1>";
-}
+
+<?php 
+  
+$file = $_GET["file"] .".pdf"; 
+  
+// We will be outputting a PDF 
+header('Content-Type: application/pdf'); 
+  
+// It will be called downloaded.pdf 
+header('Content-Disposition: attachment; filename="gfgpdf.pdf"'); 
+  
+$imagpdf = file_put_contents($image, file_get_contents($file));  
+  
+echo $imagepdf; 
 ?>
