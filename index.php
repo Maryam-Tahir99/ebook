3e99b748fdf4787ec3bc41618a4db58503f8b7ab
Maryam-Tahir99 <61540672+Maryam-Tahir99@users.noreply.github.com>
2020-08-23 22:57:14 +0500

    Update index.php

--- index.php
+++ index.php
@@ -7,8 +7,8 @@ require "./template/header.php";  ?>
                 <div class="col-md-6 intro-heading">
                     <h1 class="brand-heading">Welcome</h1>
                     <p class="intro-text">Explore the world, and find out what you don't know by reading.</p>
-                    <a href="register.php">
-                    <button href="register.php">Sign Up</button></a>
+                    <form>
+                        <button formaction="register.php">Sign Up</button></a></form>
                 </div>
                 <div class="col-md-6 intro-image">
                     <img src="images/banner.png" alt="">
